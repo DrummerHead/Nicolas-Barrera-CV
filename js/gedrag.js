6312c599b243100085a080e270473def89fada55
DrummerHead <drummerhead@gmail.com>
2012-09-25 21:08:52 -0300

    Calculate elapsed time on current job

--- js/gedrag.js
+++ js/gedrag.js
@@ -2,35 +2,49 @@ Zepto(function($){
 
 
 
+
 // Initial vars
-var asides = $('section aside');
+var $asides = $('section aside');
 var $expander = "<a href='#' class='expander'>Read More</a>";
+var $elapsed = $('#elapsed');
 
 
-// Adding elements to aside
-asides.each(function(){
+// Collapsing aside
+$asides.each(function(){
   var me = $(this);
   me.addClass('collapsed');
   me.after($expander);
 });
 
 
-// Append behaviour to .expander trigger
+// Behaviour for .expander trigger
 $('section').on('click', '.expander', function(e){
   e.preventDefault();
   var me = $(this);
-  console.log(me);
   me.prev().removeClass('collapsed').addClass('opened');
   me.remove();
 });
 
 
+// Time elapsed in current job
+var miliInAMonth = 2629741666;
 
+var currentJobStartDate = new Date(2012, 8, 1).getTime();
+var today = new Date().getTime();
+var elapsed = today - currentJobStartDate;
 
+var elapsedMonthsTotal = Math.floor(elapsed / miliInAMonth);
+var elapsedYears = Math.floor(elapsedMonthsTotal / 12);
+var elapsedMonths = elapsedMonthsTotal % 12;
 
+var dateToString = function(date, string){
+  var dateString = (date ? date + ' ' + string + (date > 1 ? 's' : '') : '');
+  return dateString;
+};
 
+var elapsedString = dateToString(elapsedYears, 'year') + ' ' + dateToString(elapsedMonths, 'month');
 
-
+$elapsed.text(elapsedString);
 
 
 
